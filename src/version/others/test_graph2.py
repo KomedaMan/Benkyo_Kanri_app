bbbaf65e32fdc737322039d57b432bcb6106474d
KomedaMan <skp51345@gmail.com>
2026-08-27 23:14:13 +0900

    graphのテスト

--- src/version/others/test_graph2.py
+++ src/version/others/test_graph2.py
@@ -1,9 +1,9 @@
-# pyplotインターフェース
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 1, 100)
 y = x ** 2
+fig = plt.figure()
+ax = fig.add_subplot(111)
 plt.plot(x, y)
 plt.show()
-
